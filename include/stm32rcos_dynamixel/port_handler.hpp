35bef99cb1e279d557d2189b5e0a21396db6858d
Rin Iwai <rin@eyrin.jp>
2025-04-11 01:37:53 +0900

    init

---
+++ include/stm32rcos_dynamixel/port_handler.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <stm32rcos/core.hpp>
+
+#include <stm32rcos/peripheral/uart.hpp>
+
+#include "port_handler.h"
+
+namespace stm32rcos_dynamixel {
+
+class PortHandler : public dynamixel::PortHandler {
+public:
+  PortHandler(stm32rcos::peripheral::UART &uart) : uart_{uart} {
+    is_using_ = false;
+  }
+
+  virtual ~PortHandler() { closePort(); }
+
+  bool openPort() {
+    return false; // not implemented
+  }
+
+  void closePort() {
+    // not implemented
+  }
+
+  void clearPort() { uart_.flush(); }
+
+  void setPortName(const char *) {
+    // not implemented
+  }
+
+  char *getPortName() {
+    return nullptr; // not implemented
+  }
+
+  bool setBaudRate(const int baudrate) {
+    baudrate_ = baudrate;
+    tx_time_per_byte = (1000.0 / (double)baudrate) * 10.0;
+    return true;
+  }
+
+  int getBaudRate() { return baudrate_; }
+
+  int getBytesAvailable() { return uart_.available(); }
+
+  int readPort(uint8_t *packet, int length) {
+    if (!uart_.receive(packet, length, packet_timeout_)) {
+      is_timeout_ = true;
+      return 0;
+    }
+    return length;
+  }
+
+  int writePort(uint8_t *packet, int length) {
+    if (!uart_.transmit(packet, length, osWaitForever)) {
+      return 0;
+    }
+    return length;
+  }
+
+  void setPacketTimeout(uint16_t packet_length) {
+    packet_timeout_ = (tx_time_per_byte * (double)packet_length) +
+                      (LATENCY_TIMER * 2.0) + 2.0;
+  }
+
+  void setPacketTimeout(double msec) { packet_timeout_ = msec; }
+
+  bool isPacketTimeout() {
+    if (is_timeout_) {
+      is_timeout_ = false;
+      return true;
+    }
+    return false;
+  }
+
+private:
+  static constexpr int LATENCY_TIMER = 4;
+
+  stm32rcos::peripheral::UART &uart_;
+  int baudrate_ = 0;
+  double packet_timeout_ = 0.0;
+  double tx_time_per_byte = 0.0;
+  bool is_timeout_ = false;
+};
+
+} // namespace stm32rcos_dynamixel
---
+++ src/port_handler.cpp
@@ -0,0 +1,7 @@
+#include "port_handler.h"
+
+using namespace dynamixel;
+
+PortHandler *PortHandler::getPortHandler(const char *) {
+  return nullptr; // not implemented
+}
